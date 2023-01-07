35cb704f3068b16272f2c2472018f11b470a0e85
Mustafa Adnan Tatlici <m.adnantatlici@gmail.com>
2023-01-07 22:13:03 +0300

    tableView

---
+++ ArtBookProject/DetailsVC.swift
@@ -0,0 +1,30 @@
+//
+//  DetailsVC.swift
+//  ArtBookProject
+//
+//  Created by Mustafa Adnan Tatlıcı on 7.01.2023.
+//
+
+import UIKit
+
+class DetailsVC: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameTF: UITextField!
+    @IBOutlet weak var artistTF: UITextField!
+    @IBOutlet weak var yearTF: UITextField!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+
+    }
+    
+
+    
+    
+
+}
