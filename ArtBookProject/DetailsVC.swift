211d7c335573e4699c6dcb8b3372ccd892d8c643
Mustafa Adnan Tatlici <m.adnantatlici@gmail.com>
2023-01-10 01:46:31 +0300

    CoreData

--- ArtBookProject/DetailsVC.swift
+++ ArtBookProject/DetailsVC.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import CoreData
 
 class DetailsVC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
@@ -49,5 +50,31 @@ class DetailsVC: UIViewController, UIImagePickerControllerDelegate, UINavigation
     }
     
     
-
+    @IBAction func saveButtonClicked(_ sender: Any) {
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        let newPainting = NSEntityDescription.insertNewObject(forEntityName: "Paintings", into: context)
+        
+        //Attributes
+        
+        newPainting.setValue(nameTF.text!, forKey: "name")
+        newPainting.setValue(artistTF.text, forKey: "artist")
+        if let year = Int(yearTF.text!) {
+            
+            newPainting.setValue(year, forKey: "year")
+        }
+        newPainting.setValue(UUID(), forKey: "id")
+        
+        let data = imageView.image!.jpegData(compressionQuality: 0.5)
+        newPainting.setValue(data, forKey: "image")
+        
+        do {
+            try context.save()
+            print("başarılı")
+        } catch {
+            print("error")
+        }
+    }
+    
 }
